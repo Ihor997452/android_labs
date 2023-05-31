58d49ea4126c7c5179a229079d3f4c1fe179f9cf
namelless <namelless@example.com>
2023-05-31 10:14:17 +0300

    lab2: created basic layout

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -2,42 +2,10 @@ package com.example.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        findViewById<Button>(R.id.rollButton).setOnClickListener { roll() }
-    }
-
-    fun roll() {
-        val dice = Dice(6)
-        val diceRoll = dice.roll()
-
-        val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-        toast.show()
-
-        val drawableResource = when(diceRoll) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            6 -> R.drawable.dice_6
-            else -> R.drawable.ic_launcher_foreground
-        }
-
-        findViewById<ImageView>(R.id.diceImage).setImageResource(drawableResource)
-    }
-}
-
-class Dice(private val numSides: Int) {
-    fun roll(): Int {
-        return (1..numSides).random()
     }
 }
